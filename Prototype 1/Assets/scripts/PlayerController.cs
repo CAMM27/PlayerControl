12bd8f4d543ffb275625cbd82a30041f33209ea7
CAMM27 <oneillc@roverkids.org>
2024-11-15 13:54:18 -0500

    moving forwards and backwards

--- Prototype 1/Assets/scripts/PlayerController.cs
+++ Prototype 1/Assets/scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,8 +16,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // we'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
